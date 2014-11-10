bf7a505d7b8c833ed2ad8ad892199f71f269cd4c
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2014-11-10 14:49:30 +0200

    Convert HangOut Activities Csv To Json

--- HttpDataStore.Tester/Program.cs
+++ HttpDataStore.Tester/Program.cs
@@ -13,7 +13,7 @@ namespace HttpDataStore.Tester
             //new BlobStore("http://localhost/HDataStore/").Save(@"C:\WORK\ev\Docs\arch\iview-main.png");
 
             ConvertHangOutPlacesCsvToJson(@"C:\Users\dan.hintea\Downloads\Mobile app contest - Places.csv", @"C:\Users\dan.hintea\Downloads\Mobile app contest - Places.json");
-
+            ConvertHangOutActivitiesCsvToJson(@"C:\Users\dan.hintea\Downloads\Mobile app contest - Activities.csv", @"C:\Users\dan.hintea\Downloads\Mobile app contest - Activities.json");
             Console.WriteLine("\r\nDone. Press key to exit.");
             Console.ReadKey();
         }
@@ -45,10 +45,40 @@ namespace HttpDataStore.Tester
                         if(string.IsNullOrWhiteSpace(value)){
                             return new string[0];
                         }
-                        return value.Split(' ').Select(v => v.Trim()).ToArray();
+                        return value.Split(' ').Where(v => !string.IsNullOrWhiteSpace(v)).Select(v => v.Trim()).ToArray();
                     }
                 },
             }, entry => string.IsNullOrWhiteSpace(entry["name"]));
+        }
+
+        private static void ConvertHangOutActivitiesCsvToJson(string csvFile, string jsonFile)
+        {
+            ConvertCsvToJson.FromFile(csvFile, jsonFile, new Dictionary<string, string>{
+                { "Title", "title" },
+                { "Category", "category" },
+                { "Keywords (space separated)", "keywords" },
+                { "Description", "description" },
+                { "Tags (space separated - used to link activity to place)", "tags" },
+                { "Picture", "imageUrl" },
+            }, new Dictionary<string, Func<string, dynamic>> { 
+                { "keywords", value => 
+                    {
+                        if (string.IsNullOrWhiteSpace(value))
+                        {
+                            return new string[0];
+                        }
+                        return value.Split(' ').Where(v => !string.IsNullOrWhiteSpace(v)).Select(v => v.Trim()).ToArray();
+                    }
+                },
+                { "tags", value => 
+                    {
+                        if(string.IsNullOrWhiteSpace(value)){
+                            return new string[0];
+                        }
+                        return value.Split(' ').Where(v => !string.IsNullOrWhiteSpace(v)).Select(v => v.Trim()).ToArray();
+                    }
+                },
+            }, entry => string.IsNullOrWhiteSpace(entry["title"]));
         }
     }
 }
