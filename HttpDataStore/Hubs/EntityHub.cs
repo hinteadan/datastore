4785384ac95f94dcd9c6437dc5955010c98f5289
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2014-12-17 11:19:13 +0200

    Add basic scaffold for SignalR

---
+++ HttpDataStore/Hubs/EntityHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace HttpDataStore.Hubs
+{
+    public class EntityHub : Hub
+    {
+        public void Ping()
+        {
+            Clients.Caller.pong(DateTime.Now);
+        }
+    }
+}
